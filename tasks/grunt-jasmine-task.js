e4cd37323e972236056dfd9091ebf67b8c314245
Andrew Jones <ajones782@gmail.com>
2015-06-09 15:58:25 +0100

    initial commit

---
+++ tasks/grunt-jasmine-task.js
@@ -0,0 +1,13 @@
+module.exports = {
+    grunt.registerMultiTask('grunt-jasmine', function(done) {
+        var jasmine = new (require('jasmine'))();
+        var done = this.async();
+
+        this.data['spec_dir'] = this.data['spec_dir'] || '.';
+        this.data['spec_files'] = this.data['spec_files'] || ['**/*[sS]pec.js'];
+
+        jasmine.loadConfig(this.data);
+        jasmine.onComplete(done);
+        jasmine.execute();        
+    });
+};
