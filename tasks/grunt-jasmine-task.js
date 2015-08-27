965d1d334e9fd67fb5f5e6f19b992f9266db3b0f
Steven Cook <stevenjamescook@gmail.com>
2015-08-27 16:31:46 +0100

    correction to the package

--- tasks/grunt-jasmine-task.js
+++ tasks/grunt-jasmine-task.js
@@ -2,16 +2,25 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('jasmine', function(done) {
         var jasmine = new (require('jasmine'))();
         var reporters = require('jasmine-reporters');
+
         var junitReporter = new reporters.JUnitXmlReporter({
-            savePath: __dirname,
+            savePath: process.cwd() + '/reports',
             consolidateAll: false
         });
+
+        var terminalReporter = new reporters.TerminalReporter({
+            verbosity: 3,
+            color: true,
+            showStack: true
+        });
+
         var done = this.async();
 
         this.data['spec_dir'] = this.data['spec_dir'] || '.';
         this.data['spec_files'] = this.data['spec_files'] || ['**/*[sS]pec.js'];
 
         jasmine.loadConfig(this.data);
+        jasmine.addReporter(terminalReporter);
         jasmine.addReporter(junitReporter);
         jasmine.onComplete(done);
         jasmine.execute();
