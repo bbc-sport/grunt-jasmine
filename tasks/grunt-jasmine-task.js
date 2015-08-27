7ab72e2f1462c502c6b3966eca3afe4134916b24
Steven Cook <stevenjamescook@gmail.com>
2015-08-27 14:58:33 +0100

    Added  jasmine-reporters Configured JUnit Repsorter

--- tasks/grunt-jasmine-task.js
+++ tasks/grunt-jasmine-task.js
@@ -1,13 +1,19 @@
 module.exports = function(grunt) {
     grunt.registerMultiTask('jasmine', function(done) {
         var jasmine = new (require('jasmine'))();
+        var reporters = require('jasmine-reporters');
+        var junitReporter = new reporters.JUnitXmlReporter({
+            savePath: __dirname,
+            consolidateAll: false
+        });
         var done = this.async();
 
         this.data['spec_dir'] = this.data['spec_dir'] || '.';
         this.data['spec_files'] = this.data['spec_files'] || ['**/*[sS]pec.js'];
 
         jasmine.loadConfig(this.data);
+        jasmine.addReporter(junitReporter);
         jasmine.onComplete(done);
-        jasmine.execute();        
+        jasmine.execute();
     });
 };
